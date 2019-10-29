efd9813cbd9d6c6d71533a7594dc5cae53de7893
Jacob Carson <jaycar641@yahoo.com>
2019-10-29 14:04:13 -0500

    Initial Set up

---
+++ TrashCollectorProject/Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorProject.Models
+{
+    public class Customer
+    {
+        //Customize User Registration with adding username and Role in article
+
+        [Key]
+        public int Id { get; set; }
+
+        public string firstName;
+        public string lastName;
+        public string streetAddress;
+        public string city;
+        public string state;
+        public int zipcode;
+        public string pickupDay;
+        public int balance;
+        public int monthlyRate;
+        public string startDate;
+        public string endDate;
+               
+
+    }
+}
---
+++ TrashCollectorProject/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorProject.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string firstName;
+        public string lastName;
+        public int zipcode;
+    }
+}
